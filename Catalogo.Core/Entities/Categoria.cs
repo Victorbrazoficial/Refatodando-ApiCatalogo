4857dd73e6c7973aa190c57a20b4ea96cd45b0ad
Victor Braz <vic_braz@outlook.com>
2022-08-17 11:33:46 -0300

    Adicionar arquivos de projeto.

---
+++ Catalogo.Core/Entities/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Catalogo.Core.Entities
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string? ImagemUrl { get; set; }
+        public ICollection<Produto>? Produtos { get; set; }
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+    }
+}
