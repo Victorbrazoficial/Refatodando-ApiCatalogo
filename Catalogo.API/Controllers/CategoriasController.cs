5168721b30981b262ee00af07ed27fa1ef042c93
Victor Braz <vic_braz@outlook.com>
2022-08-17 18:35:26 -0300

    Preenchendo CategoriasController

---
+++ Catalogo.API/Controllers/CategoriasController.cs
@@ -0,0 +1,41 @@
+﻿using Catalogo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalogo.API.Controllers
+{
+    [Route("api/categorias")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult GetCategorias()
+        {    
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(CategoriaModel categoria)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = categoria.IdCategoria }, categoria);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, CategoriaUpdate categoria)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ Catalogo.API/Controllers/ProdutosController.cs
@@ -0,0 +1,39 @@
+﻿using Catalogo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalogo.API.Controllers
+{
+    [Route("api/produtos")]
+    public class ProdutosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetProdutos(string query)
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetById(int id)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(ProdutoModel produto)
+        {
+            return CreatedAtAction(nameof(GetById), new { id = produto.IdProduto }, produto);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, ProdutoUpdate produto)
+        {
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
