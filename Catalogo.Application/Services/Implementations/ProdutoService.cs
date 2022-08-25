754844e073bac115b919a93492d324e815c3aeb9
Victor Braz <vic_braz@outlook.com>
2022-08-24 21:44:53 -0300

    Adicionando Logica de busca no ProdutoService

--- Catalogo.Application/Services/Implementations/ProdutoService.cs
+++ Catalogo.Application/Services/Implementations/ProdutoService.cs
@@ -1,12 +1,66 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Catalogo.Application.InputModels;
+using Catalogo.Application.Services.Interfaces;
+using Catalogo.Application.ViewModels;
+using Catalogo.Infrastructure.Persistence;
 
 namespace Catalogo.Application.Services.Implementations
 {
-    public class ProdutoService
+    public class ProdutoService : IProdutoService
     {
-        
+
+        private readonly CatalogoDbContext _catalogoDbContext;
+
+        public ProdutoService(CatalogoDbContext catalogoDbContext)
+        {
+            _catalogoDbContext = catalogoDbContext;
+        }
+
+        public void Atualiza(AtualizaProdutoInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Cadastra(NovoProdutoInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProdutoViewModel> GetAll(string query)
+        {
+            var produtos = _catalogoDbContext.Produtos;
+            var produtoViewModel = produtos
+                .Select(x => new ProdutoViewModel() { Nome = x.Nome, Descricao = x.Descricao, Estoque = x.Estoque, ImagemUrl = x.ImagemUrl })
+                .OrderBy(x => x.Nome)
+                .ToList();
+
+            return produtoViewModel;
+        }
+
+        public DetalhesProdutoIdViewModel GetById(int id)
+        {
+            var produto = _catalogoDbContext.Produtos.SingleOrDefault(p => p.ProdutoId == id);
+
+            if (produto is null)
+                return null;
+
+            var detalhesProdutoId = new DetalhesProdutoIdViewModel()
+            {
+                ProdutoId = produto.ProdutoId,
+                Nome = produto.Nome,
+                Preco = produto.Preco,
+                Descricao = produto.Descricao,
+                ImagemUrl = produto.ImagemUrl,
+                Estoque = produto.Estoque,
+                DataCadastro = produto.DataCadastro,
+                CategoriaId = produto.CategoriaId          
+            };
+
+            return detalhesProdutoId;
+        }
     }
 }
