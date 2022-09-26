533dce16db49e7ae84dd4f3f5883777a3b586c79
Victor Braz <vic_braz@outlook.com>
2022-09-26 19:11:55 -0300

    refatorando categoriaService para CQRS Categoria Queries

---
+++ Catalogo.Application/Queries/CategoriaQuerie/GetByIdDetalhesCategoria.cs
@@ -0,0 +1,10 @@
+﻿using Catalogo.Application.ViewModels;
+using MediatR;
+
+namespace Catalogo.Application.Queries.CategoriaQuerie
+{
+    public class GetByIdDetalhesCategoria : IRequest<DetalhesCategoriaIdViewModel>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Catalogo.Application/Queries/CategoriaQuerie/GetByIdDetalhesCategoriaHandler.cs
@@ -0,0 +1,35 @@
+﻿using Catalogo.Application.ViewModels;
+using Catalogo.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogo.Application.Queries.CategoriaQuerie
+{
+    public class GetByIdDetalhesCategoriaHandler : IRequestHandler<GetByIdDetalhesCategoria, DetalhesCategoriaIdViewModel>
+    {
+        public CatalogoDbContext _catalogoDbContext;
+
+        public GetByIdDetalhesCategoriaHandler(CatalogoDbContext catalogoDbContext)
+        {
+            _catalogoDbContext = catalogoDbContext;
+        }
+
+        public async Task<DetalhesCategoriaIdViewModel> Handle(GetByIdDetalhesCategoria request, CancellationToken cancellationToken)
+        {
+            var categoria = await _catalogoDbContext.Categorias.SingleOrDefaultAsync(x => x.CategoriaId == request.Id);
+
+            if (categoria is null)
+                return null;
+
+            var detalhesViewModel = new DetalhesCategoriaIdViewModel()
+            {
+                Nome = categoria.Nome,
+                ImagemUrl = categoria.ImagemUrl,
+                Id = categoria.CategoriaId,
+                Produtos = categoria.Produtos
+            };
+
+            return detalhesViewModel;
+        }
+    }
+}
