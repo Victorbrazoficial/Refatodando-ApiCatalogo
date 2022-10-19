02baf36f65e8713ef5acbc5138cd699a9c581cdd
Victor Braz <vic_braz@outlook.com>
2022-10-19 07:35:50 -0300

    Teste unitario CadastrarCategoriaCommandHandlerTest

---
+++ Catalogo.UnitTests/Application/Commands/CadastrarCategoriaCommandHandlerTest.cs
@@ -0,0 +1,34 @@
+﻿using Catalogo.Application.Commands.CategoriaCommand;
+using Catalogo.Core.Entities;
+using Catalogo.Core.Repositories;
+using Moq;
+
+namespace Catalogo.UnitTests.Application.Commands
+{
+    public class CadastrarCategoriaCommandHandlerTest
+    {
+        [Fact]
+        public async Task entrouDados_Executado_RetornarCategoriaId()
+        {
+            //Arranger
+            var categoriaRepositoryMock = new Mock<ICategoriaRepository>();
+            var cadastarCategoriaCommand = new CadastrarCategoriaCommand()
+            {
+                CategoriaId = 1,
+                Nome = "Teste nome 1",
+                ImagemUrl = "teste1.jpg"
+            };
+
+            var categoriaCadastroCommandHandler = new CadastrarCategoriaCommandHandler(categoriaRepositoryMock.Object);
+
+            //Act
+            var id = await categoriaCadastroCommandHandler.Handle(cadastarCategoriaCommand, new CancellationToken());
+
+            //Assert
+            Assert.True(id > 0);
+            Assert.Equal(1, id);
+
+            categoriaRepositoryMock.Verify(c => c.Cadastrar(It.IsAny<Categoria>()), Times.Once());
+        }
+    }
+}
