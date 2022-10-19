f064762cd2392e33061cf81f1c4acaf7b25a60e3
Victor Braz <vic_braz@outlook.com>
2022-10-19 08:37:08 -0300

    Teste unitario UpdateCategoriaCommandHandlerTest

---
+++ Catalogo.UnitTests/Application/Commands/UpdateCategoriaCommandHandlerTest.cs
@@ -0,0 +1,33 @@
+﻿using Catalogo.Application.Commands.CategoriaCommand;
+using Catalogo.Core.Entities;
+using Catalogo.Core.Repositories;
+using Moq;
+
+namespace Catalogo.UnitTests.Application.Commands
+{
+    public class UpdateCategoriaCommandHandlerTest
+    {
+        [Fact]
+        public async Task EntrouDados_Executado_RetornarSucesso()
+        {
+            //Arranger
+            var categoriaRepositoryMock = new Mock<ICategoriaRepository>();
+            var updateCategoriaCommand = new UpdateCategoriaCommand();
+            categoriaRepositoryMock.Setup(c => c.GetByIdDetalhesAsync(updateCategoriaCommand.Id).Result).Returns(new Categoria()
+            {
+                CategoriaId = 2,
+                Nome = "Teste nome 2",
+                ImagemUrl = "teste2.jpg",
+                Produtos = new List<Produto>()
+            });
+            var updateCategoriaCommandHandler = new UpdateCategoriaCommandHandler(categoriaRepositoryMock.Object);
+
+            //Act
+            await updateCategoriaCommandHandler.Handle(updateCategoriaCommand, new CancellationToken());
+            
+            //Assert
+            categoriaRepositoryMock.Verify(c => c.GetByIdDetalhesAsync(It.IsAny<int>()), Times.Once());
+            categoriaRepositoryMock.Verify(c => c.SaveChangeAsync(), Times.Once());
+        }
+    }
+}
