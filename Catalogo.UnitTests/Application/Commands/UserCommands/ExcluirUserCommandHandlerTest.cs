c73030e9929ab449be6781f2718e681dd6646650
Victor Braz <vic_braz@outlook.com>
2022-10-24 08:35:09 -0300

    Teste unitario ExcluirUserHandlerTest

---
+++ Catalogo.UnitTests/Application/Commands/UserCommands/ExcluirUserCommandHandlerTest.cs
@@ -0,0 +1,24 @@
+﻿using Catalogo.Application.Commands.UserCommand;
+using Catalogo.Core.Repositories;
+using Moq;
+
+namespace Catalogo.UnitTests.Application.Commands.UserCommands
+{
+    public class ExcluirUserCommandHandlerTest
+    {
+        [Fact]
+        public async Task ExcluirUser_Executado_VerificaSeOMetodoFoiChamado()
+        {
+            //Arranger
+            var userRepository = new Mock<IUserRepository>();
+            var excluirUserCommand = new ExcluirUserCommand();
+            var excluirUserCommandHandler = new ExcluirUserCommandHandler(userRepository.Object);
+
+            //Act
+            await excluirUserCommandHandler.Handle(excluirUserCommand, new CancellationToken());
+
+            //Assert
+            userRepository.Verify(u => u.Excluir(It.IsAny<int>()), Times.Once());
+        }
+    }
+}
