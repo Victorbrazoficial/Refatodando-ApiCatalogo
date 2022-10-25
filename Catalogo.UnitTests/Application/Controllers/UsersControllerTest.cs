3425f1d8c80d7b74e31bccb87211ca443c9a65c3
Victor Braz <vic_braz@outlook.com>
2022-10-25 08:12:27 -0300

    Testte unitario de Cadastrar em UserController

---
+++ Catalogo.UnitTests/Application/Controllers/UsersControllerTest.cs
@@ -0,0 +1,30 @@
+﻿using Catalogo.API.Controllers;
+using Catalogo.Application.Commands.UserCommand;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace Catalogo.UnitTests.Application.Controllers
+{
+    public class UsersControllerTest
+    {
+        private readonly Mock<IMediator> _iMediator = new Mock<IMediator>(); 
+        [Fact]
+        public async Task Cadastrar_Executado_Retorna201()
+        {
+            var controller = new UsersController(_iMediator.Object);
+            var cadastrarUserCommand =  new CadastrarUserCommand()
+            {
+                Id = 1,
+                NomeCompleto = "Usuario 1",
+                Email = "usuario1@teste.com",
+                Password = "123456789",
+                DataAniversario = DateTime.Now,
+            };
+            var result = await controller.Cadastrar(cadastrarUserCommand);
+
+            Assert.NotNull(result);
+            Assert.Equal(201, (result as CreatedAtActionResult).StatusCode);
+        }
+    }
+}
